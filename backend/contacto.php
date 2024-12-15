ab7fde201e548c4886ac6c20a18f8fedcbaecf32
JorgeAlfaro29 <jorgealfarog29@hotmail.com>
2024-12-15 02:33:23 -0600

    Contacto

---
+++ backend/contacto.php
@@ -0,0 +1,64 @@
+<?php
+    class contacto {
+        public $nombre;
+        public $primerApellido;
+        public $segundoApellido;
+        public $email;
+        public $numeroTelefono;
+        public $mensaje;
+
+        public function __construct($nombre, $primerApellido, $segundoApellido, $email,$numeroTelefono, $mensaje ) {
+            $this->nombre = $nombre;
+            $this->primerApellido = $primerApellido;
+            $this->segundoApellido = $segundoApellido;
+            $this->email = $email;
+            $this->numeroTelefono = $numeroTelefono;
+            $this->mensaje = $mensaje;
+        }
+    }
+
+    interface UserDAO {
+        public function InsertarContacto($nombre, $primerApellido, $segundoApellido, $email,$numeroTelefono, $mensaje);
+    }
+
+    class UserDAOSpImpl implements UserDAO {
+
+        private $conn;
+
+        public function __construct() {
+            $this->conn = new mysqli("localhost", "jorge", "1234", "proyecto");
+
+            if ($this->conn->connect_error) {
+            die("Conexión fallida: " . $this->conn->connect_error);
+        }
+        }
+
+        public function InsertarContacto($nombre, $primerApellido, $segundoApellido, $email,$numeroTelefono, $mensaje) {
+            $function_conn = $this->conn;
+            $stmt = $function_conn->prepare("CALL InsertarContacto(?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param("ssssss", $nombre, $primerApellido, $segundoApellido, $email,$numeroTelefono, $mensaje);
+            $stmt->execute();
+        }
+    }
+
+    $userDao = new UserDAOSpImpl();
+    
+    $userDao->InsertarContacto("Jorge", "Alfaro", "Perez","hola@gmail.com", "123456", "hola");
+    echo "dato ingresados correctamente"
+
+    /*if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nombre = $_POST['nombre'] ?? '';
+        $primerApellido = $_POST['primerApellido'] ?? '';
+        $segundoApellido = $_POST['segundoApellido'] ?? '';
+        $email = $_POST['email'] ?? '';
+        $numeroTelefono = $_POST['numeroTelefono'] ?? '';
+        $mensaje = $_POST['mensaje'] ?? '';
+
+        $userDao->InsertarContacto($nombre, $primerApellido, $segundoApellido, $email, $numeroTelefono, $mensaje);
+    } else {
+        echo "Error";
+    }
+        */
+
+
+?>
